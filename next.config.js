bba68bc26049e870740e594507c0e1115ce3adc4
ebagabee <gabrielhas.tech@gmail.com>
2025-01-04 07:04:32 -0300

    troca fonte

--- next.config.js
+++ next.config.js
@@ -3,20 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   typescript: {
-    // Para desenvolvimento, você pode querer mudar para false
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Para desenvolvimento, você pode querer mudar para false
     ignoreDuringBuilds: true,
   },
   images: {
     domains: ['github.com'],
-  },
-  experimental: {
-    // Desabilite isso se estiver tendo problemas com o Turbopack
-    turbo: true,
   }
+  // Removemos a seção experimental.turbo que estava causando erro
 };
 
 module.exports = nextConfig;
