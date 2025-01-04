19146af633a3171e785398d83dd78e1e459bb86b
ebagabee <gabrielhas.tech@gmail.com>
2025-01-04 05:57:39 -0300

    remove dependencia

--- src/app/(protected)/finances/page.tsx
+++ src/app/(protected)/finances/page.tsx
@@ -20,7 +20,7 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Textarea, TextArea } from "@/components/ui/textarea";
+import { Textarea } from "@/components/ui/textarea";
 import {
   Select,
   SelectContent,
